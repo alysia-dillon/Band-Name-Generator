c174912d2481e10c00e3e9bf057c8046c29f20b8
Alysia Dillon <alysia.dillon@outlook.com>
2023-01-22 23:41:59 +0000

    #100daysOfCode project

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 # will respond with 'Hello {name}' once input is answered
-print("Welcome to the Band Name Generator.")
+print("Welcome to the Band Name Generator.\nTo start, please answer the below prompts.")
 
 city = input("What city did you grow up in?\n")
 pet_name = input("What is the name of a pet?\n")
 band_name = city + ' ' + pet_name
 
 print("Your new band name is: " + band_name)
-
