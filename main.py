ea2f669c0c5fc34dcb41affd78f2b9a68b05e49d
Alysia Dillon <alysia.dillon@outlook.com>
2023-01-23 23:01:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# will respond with 'Hello {name}' once input is answered
+print("Welcome to the Band Name Generator.")
+
+city = input("What city did you grow up in?\n")
+pet_name = input("What is the name of a pet?\n")
+band_name = city + ' ' + pet_name
+
+print("Your new band name is: " + band_name)
+
